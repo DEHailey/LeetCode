21a453c7ccb1f160263f0dd5607805462c634087
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-08-27 15:13:00 +0900

    Time: 424 ms (84.10%), Space: 29.2 MB (11.34%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        num_set = set()
+        for num in nums:
+            if num in num_set:
+                return True
+            num_set.add(num)
+        return False
