5815e5696841f2dbcae6819a43bfff3ba4e776b7
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-05 12:16:38 +0900

    Time: 4304 ms (9.02%), Space: 15.8 MB (31.73%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def singleNumber(self, nums):
+        if len(nums) == 1:
+            return nums[0]
         for n in nums:
             if nums.count(n) == 1:
                 return n
