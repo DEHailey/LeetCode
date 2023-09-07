cb78df42710315b5d15f9104c87c10fddd5fc89f
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-07 15:13:25 +0900

    Time: 54 ms (9.05%), Space: 16.1 MB (7.88%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -1,5 +1,5 @@
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
@@ -9,7 +9,7 @@ class Solution:
         def helper(left, right):
             if left > right:
                 return None
-            
+
             middle = (left + right) // 2 
             if (left + right) % 2:
                 middle += 1 
@@ -18,6 +18,5 @@ class Solution:
             root.left = helper(left, middle - 1)
             root.right = helper(middle + 1, right)
             return root
-        
+
         return helper(0, len(nums) - 1)
-    
