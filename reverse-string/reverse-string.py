9d0e85c63340ec5e404b166300f15c188331374a
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-11 11:43:18 +0900

    Time: 133 ms, Memory: 21 MB - LeetHub

---
+++ reverse-string/reverse-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseString(self, s):
+        left = 0
+        right = len(s) -1
+        
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+            
+        return s == len(s)
+            
+        
