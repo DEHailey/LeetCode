2a67ee08ecdbf0b54ada593e4e392fca8aea0c82
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-12 20:43:20 +0900

    Time: 1232 ms (79.66%), Space: 35.7 MB (5.42%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.py
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.py
@@ -2,21 +2,19 @@ class Solution:
     def getAverages(self, nums, k):
         if k == 0:
             return nums
-
         n = len(nums)
-        averages = [-1] * n
-
-        if 2 * k + 1 > n:
-            return averages
-
+        averages = [-1]*n
+        
         prefix = [0] * (n + 1)
         for i in range(n):
             prefix[i + 1] = prefix[i] + nums[i]
-
-        for i in range(k, n - k):
-            leftBound, rightBound = i - k, i + k
-            subArraySum = prefix[rightBound + 1] - prefix[leftBound]
-            average = subArraySum // (2 * k + 1)
+            
+        for i in range(k, n-k):
+            leftbound, rightbound = i-k, i+k
+            subArraySum = prefix[rightbound+1]-prefix[leftbound]
+            average = subArraySum // (k * 2 + 1)
             averages[i] = average
-
+            
         return averages
+            
+            
