f055fb2ffd35061313c11359edb1b357746be8c2
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-13 13:16:35 +0900

    Time: 112 ms (85.64%), Space: 14.4 MB (91.90%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        index = 0
+
+        for n in nums:
+            if n != 0:
+                nums[index] = n
+                index += 1
+                
+        while index < len(nums):
+            nums[index] = 0
+            index += 1
+
+            
+        return nums
+                
+        
