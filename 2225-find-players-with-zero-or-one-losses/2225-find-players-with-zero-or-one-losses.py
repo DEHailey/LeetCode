284e6e62424e7bbcb63f2df255113caab69312b8
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-15 18:55:17 +0900

    Time: 1362 ms (86.56%), Space: 72.1 MB (91.48%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findWinners(self, matches):
+        zero_loss = set()
+        one_loss = set()
+        more_losses = set()
+        
+        for winner, loser in matches:
+            if (winner not in one_loss) and (winner not in more_losses):
+                zero_loss.add(winner)
+            if loser in zero_loss:
+                zero_loss.remove(loser)
+                one_loss.add(loser)
+            elif loser in one_loss:
+                one_loss.remove(loser)
+                more_losses.add(loser)
+            elif loser in more_losses:
+                continue
+            else:
+                one_loss.add(loser)
+                
+        return [sorted(list(zero_loss)), sorted(list(one_loss))]
