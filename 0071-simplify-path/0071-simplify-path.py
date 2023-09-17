91f7209af5f8f2f7183739466cfc23f098b8843c
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-17 20:49:09 +0900

    Time: 7 ms (99.42%), Space: 13.4 MB (74.37%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        stack = []
+        
+        for portion in path.split('/'):
+            if portion == '..':
+                if stack:
+                    stack.pop()
+            elif portion == '.' or not portion:
+                continue
+            else:
+                stack.append(portion)
+                
+        return '/' + '/'.join(stack)
