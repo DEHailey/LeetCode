ec6aa3ddde0129456792a8b09462341dad2d6cf9
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-17 21:29:17 +0900

    Time: 21 ms (34.42%), Space: 13.3 MB (85.46%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def makeGood(self,s):
+        while len(s) > 1:
+            find = False
+            
+            for i in range(len(s)-1):
+                curr_char, next_char = s[i], s[i+1]
+                
+                if abs(ord(curr_char)- ord(next_char)) ==  32:
+                    s = s[:i] + s[i+2:]
+                    find = True
+                    break
+                    
+            if not find:
+                break
+                
+        return s
+        
