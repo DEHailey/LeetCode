e5278aab202cc993c3b1789f02515c7548fc7d8a
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-25 09:48:07 +0900

    Time: 64 ms, Memory: 13.7 MB - LeetHub

---
+++ longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def answerQueries(self, nums, queries):
+        nums.sort()
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
+            
+        ans = []
+        for q in queries:
+            index = bisect.bisect_right(nums, q)
+            ans.append(index)
+            
+        return ans
+        
