a38e8870cb1f6694c70d846f94d3b4963cbfb9d1
Hailey <111622572+DEHailey@users.noreply.github.com>
2023-09-27 07:26:40 +0900

    Time: 2203 ms (18.83%), Space: 15.5 MB (44.88%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -4,33 +4,35 @@ class Solution(object):
             return 0 <= row < m and 0 <= col < n
         
         def check(effort):
-            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            seen = {(0, 0)}
-            stack = [(0, 0)]
+            directions = [(0,1),(1,0),(0,-1),(-1,0)]
+            seen = {(0,0)}
+            stack = [(0,0)]
             
             while stack:
                 row, col = stack.pop()
-                if (row, col) == (m - 1, n - 1):
+                if (row, col) == (m-1, n-1):
                     return True
                 
                 for dx, dy in directions:
-                    next_row, next_col = row + dy, col + dx
+                    next_row, next_col = row + dx, col + dy
                     if valid(next_row, next_col) and (next_row, next_col) not in seen:
-                        if abs(heights[next_row][next_col] - heights[row][col]) <= effort:
+                        if abs(heights[next_row][next_col]- heights[row][col]) <= effort:
                             seen.add((next_row, next_col))
                             stack.append((next_row, next_col))
-            
             return False
         
         m = len(heights)
         n = len(heights[0])
+        
         left = 0
         right = max(max(row) for row in heights)
         while left <= right:
             mid = (left + right) // 2
             if check(mid):
-                right = mid - 1
+                right = mid -1
             else:
                 left = mid + 1
         
-        return left
+        return left 
+                
+                        
