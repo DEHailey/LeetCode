cde05f026378720b9bae4dcbf3746fc1564d9200
DEHailey <111622572+DEHailey@users.noreply.github.com>
2023-10-04 15:21:36 +0900

    Time: 53 ms (18.86%), Space: 17.4 MB (91.11%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,17 +6,20 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root, targetSum):
-        def dfs(node, curr):
-            if not node:
-                return False
+        if not root:
+            return False
 
+        stack = [(root, 0)]
+        while stack:
+            node, curr = stack.pop()
             if node.left == None and node.right == None:
-                return (curr + node.val) == targetSum
+                if (curr + node.val) == targetSum:
+                    return True
 
             curr += node.val
-            left = dfs(node.left, curr)
-            right = dfs(node.right, curr)
-            
-            return left or right
-        
-        return dfs(root, 0)
+            if node.left:
+                stack.append((node.left, curr))
+            if node.right:
+                stack.append((node.right, curr))
+
+        return False
