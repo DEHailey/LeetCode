d131fcafafe2c73bc40a9752346ec429f00dcba6
DEHailey <111622572+DEHailey@users.noreply.github.com>
2023-10-07 11:53:34 +0900

    Time: 202 ms (9.95%), Space: 25 MB (63.48%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -13,11 +13,9 @@ class Solution:
         if low <= root.val <= high:
             ans += root.val
             
-        if low < root.val:
-            ans += self.rangeSumBST(root.left, low, high)
-            
-        if root.val < high:
-            ans += self.rangeSumBST(root.right, low, high)
+        
+        ans += self.rangeSumBST(root.left, low, high)
+        ans += self.rangeSumBST(root.right, low, high)
             
         return ans
             
