e517e7e78255c12434d7c17a1b3a3c339d872a31
DEHailey <111622572+DEHailey@users.noreply.github.com>
2023-10-16 17:14:00 +0900

    Time: 258 ms, Memory: 23 MB - LeetHub

---
+++ jump-game-iii/jump-game-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        n = len(arr)
+        q = [start]
+        
+        while q:
+            node = q.pop()
+            if arr[node] == 0:
+                return True
+            
+            if arr[node] < 0:
+                continue
+                
+            for i in [node + arr[node], node -arr[node]]:
+                if 0 <= i < n:
+                    q.append(i)
+                    
+            arr[node] = -arr[node]
+            
+        return False
+            
