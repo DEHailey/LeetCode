619c9dcfb1708152de4a87bed70005ed972a389e
DEHailey <111622572+DEHailey@users.noreply.github.com>
2023-10-18 21:31:06 +0900

    Time: 43 ms, Memory: 16.3 MB - LeetHub

---
+++ combination-sum-iii/combination-sum-iii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+        def backtrack(remain, comb, next_start):
+            if remain == 0 and len(comb) == k:
+                ans.append(list(comb))
+                return
+            
+            elif remain < 0 and len(comb) == k:
+                return
+            
+            for i in range(next_start, 9):
+                comb.append(i+1)
+                backtrack(remain-i-1, comb, i+1)
+                comb.pop()
+                
+        backtrack(n,[],0)
+        
+        return ans
