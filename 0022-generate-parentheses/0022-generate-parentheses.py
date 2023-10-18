e0368d41fc690cffa397fbe9305bafff1e6d13d1
DEHailey <111622572+DEHailey@users.noreply.github.com>
2023-10-18 20:58:04 +0900

    Time: 44 ms (40.19%), Space: 16.7 MB (41.92%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def backtrack(curr_s, left_c, right_c):
+            if len(curr_s) == 2 * n:
+                ans.append(''.join(curr_s))
+                return
+            
+            if left_c < n:
+                curr_s.append("(")
+                backtrack(curr_s,left_c+1,right_c)
+                curr_s.pop()
+                
+            if right_c < left_c:
+                curr_s.append(")")
+                backtrack(curr_s,left_c,right_c+1)
+                curr_s.pop()
+                
+        backtrack([],0,0)
+        
+        return ans
