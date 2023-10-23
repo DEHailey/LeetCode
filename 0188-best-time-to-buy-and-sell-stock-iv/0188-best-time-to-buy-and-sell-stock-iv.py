726adf738f7e42a46450d9a53385e3c1a22c1f9d
DEHailey <111622572+DEHailey@users.noreply.github.com>
2023-10-23 12:39:44 +0900

    Time: 171 ms (44.94%), Space: 19.2 MB (50.15%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        @cache
-        def dp(i, holding, remain):
-            if i == len(prices) or remain == 0:
-                return 0
-            
-            ans = dp(i+1, holding, remain)
-            if holding:
-                ans = max(ans, prices[i] + dp(i+1, False, remain-1))
-            
-            else:
-                ans = max(ans, -prices[i] + dp(i+1, True, remain))
-                
-            return ans
-        
-        return dp(0, False, k)
+        n = len(prices)
+        dp = [[[0]*(k+1) for _ in range(2)] for _ in range(n+1)]
+        for i in range(n-1,-1,-1):
+            for remain in range(1,k+1):
+                for holding in range(2):
+                    ans = dp[i+1][holding][remain]
+                    if holding:
+                        ans = max(ans, prices[i] + dp[i+1][0][remain-1])
+                    else:
+                        ans = max(ans, -prices[i] + dp[i+1][1][remain])
+                        
+                    dp[i][holding][remain] = ans
+                    
+        return dp[0][0][k]
