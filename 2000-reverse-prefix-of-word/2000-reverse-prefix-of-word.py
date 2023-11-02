000a4612779c1933a95a61666c3fc8e39aefa323
DEHailey <111622572+DEHailey@users.noreply.github.com>
2023-11-03 01:16:02 +0900

    Time: 16 ms (43.75%), Space: 12.8 MB (100.00%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,6 +1,11 @@
 class Solution(object):
     def reversePrefix(self, word, ch):
         for i in range(len(word)):
-            if word[i]==ch:
+            if word[i] == ch:
                 return word[:i+1][::-1]+word[i+1:]
         return word
+                
+            
+                
+            
+        
