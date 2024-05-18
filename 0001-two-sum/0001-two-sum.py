5603a3f5e5c40a7bd9115ff7c308ec861f7e4250
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-05-18 12:05:10 +0900

    Time: 2105 ms (44.86%), Space: 12.7 MB (21.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        num_indices = {} 
-    
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_indices:
-                return [num_indices[complement], i]
-            num_indices[num] = i
-    
-        return None
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+                
+        
