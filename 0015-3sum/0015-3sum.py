69388cc553e10b71040024946af79de38791e75d
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-05-26 22:05:50 +0900

    Time: 6526 ms (5.01%), Space: 15.7 MB (13.31%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        h = {}
+        n = len(nums)
+        s = set()
+        
+        for i, num in enumerate(nums):
+            h[num] = i
+            
+        for i in range(n):
+            for j in range(i+1, n):
+                desired = - nums[i] - nums[j]
+                if desired in h and h[desired] != i and h[desired] != j:
+                    s.add(tuple(sorted([nums[i],nums[j],desired])))
+        
+        return s
+            
