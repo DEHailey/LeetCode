8ed5dbc366c15cee5478b93e75e1ba0a4d4bf248
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-05-31 15:00:29 +0900

    Time: 638 ms (43.29%), Space: 0B (100.00%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -2,10 +2,8 @@
 select 
     class
 from
-    (select
-        class, count(student) as num
-     from
-        Courses
-     group by class) as temp
-where
-    num >= 5
+    Courses
+group by 
+    class
+having 
+    count(student) >= 5
