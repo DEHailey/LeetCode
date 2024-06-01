ea813018d6a2748b3ec279fa8b435dd1a39d40c6
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-06-02 00:05:51 +0900

    Time: 11 ms (80.00%), Space: 11.6 MB (91.25%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.py
+++ 0266-palindrome-permutation/0266-palindrome-permutation.py
@@ -1,14 +1,4 @@
 class Solution(object):
     def canPermutePalindrome(self, s):
-        counter = collections.Counter(s)
-        
-        odd_chk = 0
-        for count in counter.values():
-            if count % 2:
-                odd_chk +=1
-            
-            if odd_chk > 1:
-                return False
-            
-        return True
+        return sum(v%2 for v in Counter(s).values()) < 2
             
