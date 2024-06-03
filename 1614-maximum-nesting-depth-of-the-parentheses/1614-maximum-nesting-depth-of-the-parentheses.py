a8c3d358f6194ce7e08b2b2b76a061564f7fc7a7
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-06-03 18:07:23 +0900

    Time: 17 ms (34.41%), Space: 11.5 MB (98.33%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxDepth(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack, res = [], 0
+	
+        for c in s:
+            if c == "(":
+                stack.append(c)
+                res = max(res, len(stack))
+            elif c == ")":
+                stack.pop()
+			
+        return res
