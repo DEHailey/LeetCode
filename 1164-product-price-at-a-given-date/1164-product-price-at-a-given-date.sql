a0bed73f45cd62a33c2256d73edee715ab7f902b
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-06-05 22:30:54 +0900

    Time: 1076 ms (36.25%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,28 +1,15 @@
 # Write your MySQL query statement below
-SELECT
-  product_id,
-  10 AS price
-FROM
-  Products
-GROUP BY
-  product_id
-HAVING
-  MIN(change_date) > '2019-08-16'
-UNION ALL
-SELECT
-  product_id,
-  new_price AS price
-FROM
-  Products
-WHERE
-  (product_id, change_date) IN (
-    SELECT
-      product_id,
-      MAX(change_date)
-    FROM
-      Products
-    WHERE
-      change_date <= '2019-08-16'
-    GROUP BY
-      product_id
-  )
+
+select product_id, 10 as price
+from Products
+group by product_id
+having min(change_date) > '2019-08-16'
+union all
+select product_id, new_price as price
+from Products
+where (product_id, change_date) 
+in (select product_id, max(change_date)
+    from Products
+    where change_date <= '2019-08-16'
+    group by product_id
+   )
