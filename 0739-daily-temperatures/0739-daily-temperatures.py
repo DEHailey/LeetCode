a210b140b97477bdd336018a994fa098dfaef3ad
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-06-08 01:01:31 +0900

    Time: 996 ms (85.62%), Space: 25.4 MB (63.14%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -12,9 +12,8 @@ class Solution(object):
         for i, t in enumerate(temp):
             while stk and stk[-1][0] < t:
                 stk_t, stk_i = stk.pop()
-                ans[stk_i] =  i - stk_i
+                ans[stk_i] = i-stk_i
             stk.append((t,i))
-                
         return ans
             
             
