3e6c10c04cd54ea123ca89de0e0c8c630db81917
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-06-11 15:39:08 +0900

    Time: 189 ms (66.63%), Space: 18.1 MB (75.20%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,13 +3,15 @@ class Solution:
         l, r = 0, len(nums)-1
         
         while l <= r:
-            m = l + (r-l) // 2
-            if nums[m] > target:
-                r = m - 1
-            elif nums[m] < target:
+            m = (l + r) // 2
+            
+            if nums[m] < target:
                 l = m + 1
+                
+            elif nums[m] > target:
+                r = m - 1
             else:
                 return m
             
         return -1
-        
+                
