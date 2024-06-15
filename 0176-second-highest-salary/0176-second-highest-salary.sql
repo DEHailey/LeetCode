85b81822f6fb02724baba3c925798047cd57620d
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-06-16 00:00:03 +0900

    Time: 433 ms (69.58%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-SELECT
-    (SELECT DISTINCT
-            Salary
-        FROM
-            Employee
-        ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1) AS SecondHighestSalary
+select ifnull(
+    (select distinct salary
+     from Employee
+     order by salary desc
+     limit 1 offset 1), null
+) as SecondHighestSalary
