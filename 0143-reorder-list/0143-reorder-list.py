0fb194d7c9aef7d8d4380c5c726895ae13ef0184
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-06-21 00:55:10 +0900

    Time: 48 ms (89.54%), Space: 24.5 MB (94.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,12 +8,14 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        # find the middle point
         slow = head
         fast = head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
+        # reversed second half
         second = slow.next
         prev = None
         slow.next = None
@@ -23,10 +25,13 @@ class Solution:
             prev = second
             second = temp
             
-        first,second = head,prev
+        # merge
+        first = head
+        second = prev
         while second:
             temp1, temp2 = first.next, second.next
             first.next = second
             second.next = temp1
             first, second = temp1, temp2
+           
             
