05ab4dda738edae686b9ba34f6e4d17d24dfe363
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-06-20 22:19:51 +0900

    Time: 48 ms (89.54%), Space: 24.4 MB (94.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,9 +8,6 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head:
-            return 
-        
         slow = fast = head
         while fast and fast.next:
             slow = slow.next
