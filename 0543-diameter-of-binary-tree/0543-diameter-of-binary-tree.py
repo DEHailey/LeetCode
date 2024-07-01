6b4dbf92df6bf579f986db825005011ff19368d6
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-01 20:38:55 +0900

    Time: 55 ms (18.58%), Space: 19.3 MB (43.46%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -9,18 +9,17 @@ class Solution:
         largest_diameter = [0]
         
         def height(root):
-            if not root:
+            if root is None:
                 return 0
             
             left_height = height(root.left)
             right_height = height(root.right)
-            diameter = left_height + right_height
             
+            diameter = left_height + right_height
             largest_diameter[0] = max(largest_diameter[0], diameter)
             
             return 1 + max(left_height, right_height)
         
         height(root)
         return largest_diameter[0]
-            
             
