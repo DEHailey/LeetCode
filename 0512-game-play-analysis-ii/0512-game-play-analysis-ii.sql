fb8479bf3e683e0214cacfa8a63bcf39b9c7b06a
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-03 15:45:35 +0900

    Time: 1278 ms (11.97%), Space: 0B (100.00%) - LeetHub

---
+++ 0512-game-play-analysis-ii/0512-game-play-analysis-ii.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select
+    distinct player_id, 
+    first_value(device_id) over(partition by player_id order by event_date) as device_id
+from Activity
+
+
