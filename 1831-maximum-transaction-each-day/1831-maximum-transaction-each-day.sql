bd147a1e401173d952f0856105bae53156536208
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-04 16:08:58 +0900

    Time: 1468 ms (5.95%), Space: 0B (100.00%) - LeetHub

--- 1831-maximum-transaction-each-day/1831-maximum-transaction-each-day.sql
+++ 1831-maximum-transaction-each-day/1831-maximum-transaction-each-day.sql
@@ -3,7 +3,6 @@ select transaction_id
 from (
     select *, dense_rank() over(partition by date(day) order by amount desc) as d
     from Transactions
-    order by day 
 ) temp
 where d = 1
 order by transaction_id asc
