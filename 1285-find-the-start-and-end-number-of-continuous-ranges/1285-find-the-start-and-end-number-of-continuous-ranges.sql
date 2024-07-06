19e83912f230c340a749960dd8cccc332e084453
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-06 22:36:54 +0900

    Time: 313 ms (99.62%), Space: 0B (100.00%) - LeetHub

---
+++ 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+select min(log_id) as start_id, max(log_id) as end_id
+from (
+    select log_id, row_number() over(order by log_id) as num
+    from Logs
+)temp
+group by log_id - num
+
+
+
+
+
+
+
+
+
+
+
+
+
