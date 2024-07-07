a2c4e247fd1c12051f16409e9e9f90b6d3d0016e
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-07 22:39:38 +0900

    Time: 1952 ms (89.93%), Space: 0B (100.00%) - LeetHub

--- 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,14 +1,12 @@
 # Write your MySQL query statement below
 select customer_id, product_id, product_name
 from (
-    select o.customer_id, o.product_id, p.product_name,
-    rank() over(partition by customer_id order by count(o.product_id) desc) as rnk
+    select o.customer_id, p.product_id, p.product_name,
+    rank() over(partition by o.customer_id order by count(p.product_id) desc) as rnk
     from Orders o
-    Join Products p
+    join Products p
     on o.product_id = p.product_id
     group by customer_id, product_id
 ) temp
 where rnk = 1
 order by customer_id, product_id
-
-
