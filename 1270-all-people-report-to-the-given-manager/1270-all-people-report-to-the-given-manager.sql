a6e5e4299c02105d4183a33b0287750a5a30930b
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-08 15:28:14 +0900

    Time: 288 ms (50.84%), Space: 0B (100.00%) - LeetHub

---
+++ 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select e1.employee_id
+from Employees e1,
+     Employees e2,
+     Employees e3
+where e1.manager_id = e2.employee_id
+  and e2.manager_id = e3.employee_id
+  and e3.manager_id = 1
+  and e1.employee_id != 1 
