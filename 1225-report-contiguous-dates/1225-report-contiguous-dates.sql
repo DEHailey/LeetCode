d9f8c3096b1a7b0b7c5d9f6000cb0b44092f07ed
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-09 17:06:15 +0900

    Time: 973 ms (85.54%), Space: 0B (100.00%) - LeetHub

--- 1225-report-contiguous-dates/1225-report-contiguous-dates.sql
+++ 1225-report-contiguous-dates/1225-report-contiguous-dates.sql
@@ -1,21 +1,16 @@
 # Write your MySQL query statement below
-select stats as period_state, min(day) as start_date, max(day) as end_date
-from (
-    select
-        day,
-        rank() over (order by day) as overall_ranking,
-        stats,
-        rk,
-        (rank() over(order by day) - rk) as inv
-    from (
-        select fail_date as day, 'failed' as stats, rank() over(order by fail_date) as rk
-        from Failed
-        where fail_date between '2019-01-01' and '2019-12-31'
-        union
-        select success_date as day, 'succeeded' as stats, rank() over(order by success_date) as rk
-        from Succeeded
-        where success_date between '2019-01-01' and '2019-12-31'
-    )t
-)c
-group by inv, stats
-order by start_date
+SELECT 'failed' AS period_state, MIN(fail_date) AS start_date, MAX(fail_date) AS end_date
+FROM (SELECT fail_date, ROW_NUMBER()OVER() AS rnk
+      FROM Failed
+      WHERE YEAR(fail_date)=2019)a
+GROUP BY DATE_SUB(fail_date,INTERVAL rnk day)
+
+UNION 
+
+SELECT 'succeeded' AS period_state, MIN(success_date) AS start_date, MAX(success_date) AS end_date
+FROM (SELECT success_date, ROW_NUMBER() OVER() AS rnk
+      FROM Succeeded
+      WHERE YEAR(success_date)=2019)b
+GROUP BY DATE_SUB(success_date, INTERVAL rnk day)
+
+ORDER BY start_date;
