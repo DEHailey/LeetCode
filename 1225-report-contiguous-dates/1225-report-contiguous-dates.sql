7cc5bb58d1abca9d508de14e78a3e087dfc7b0c9
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-09 18:28:09 +0900

    Time: 1138 ms (60.66%), Space: 0B (100.00%) - LeetHub

--- 1225-report-contiguous-dates/1225-report-contiguous-dates.sql
+++ 1225-report-contiguous-dates/1225-report-contiguous-dates.sql
@@ -2,7 +2,7 @@
 SELECT 'failed' AS period_state, MIN(fail_date) AS start_date, MAX(fail_date) AS end_date
 FROM (SELECT fail_date, ROW_NUMBER()OVER() AS rnk
       FROM Failed
-      WHERE YEAR(fail_date)=2019)a
+      WHERE YEAR(fail_date)=2019)temp
 GROUP BY DATE_SUB(fail_date,INTERVAL rnk day)
 
 UNION 
@@ -10,7 +10,7 @@ UNION
 SELECT 'succeeded' AS period_state, MIN(success_date) AS start_date, MAX(success_date) AS end_date
 FROM (SELECT success_date, ROW_NUMBER() OVER() AS rnk
       FROM Succeeded
-      WHERE YEAR(success_date)=2019)b
+      WHERE YEAR(success_date)=2019)temp
 GROUP BY DATE_SUB(success_date, INTERVAL rnk day)
 
 ORDER BY start_date;
