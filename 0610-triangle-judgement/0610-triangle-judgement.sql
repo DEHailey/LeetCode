255131b1e6a5fb06948d4f65cc15a18cd611a9b0
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-14 17:48:02 +0900

    Time: 314 ms (41.62%), Space: 0B (100.00%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-select
-    x,
-    y,
-    z,
-    (case when x + y > z and x + z > y and y + z > x then 'Yes' else 'No' end) as triangle
+select 
+    *,
+    (case when x+y > z and y+z > x and x+z > y then 'Yes' else 'No' end) as triangle
 from Triangle
 
