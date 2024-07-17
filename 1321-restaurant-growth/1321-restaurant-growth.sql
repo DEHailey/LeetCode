30311b39606f439348ce31bc4d78d00820d00afc
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-17 18:03:15 +0900

    Time: 434 ms (36.80%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
-select a.visited_on as visited_on, sum(b.dailySum) as amount, round(avg(b.dailySum),2) as average_amount 
-from 
-    (select visited_on, sum(amount) as dailySum from Customer group by visited_on) a,
-    (select visited_on, sum(amount) as dailySum from Customer group by visited_on) b
+select a.visited_on as visited_on, sum(b.dailySum) as amount, round(avg(b.dailySum),2) as average_amount
+from
+(select visited_on, sum(amount) as dailySum from Customer group by visited_on) a,
+(select visited_on, sum(amount) as dailySum from Customer group by visited_on) b
 where datediff(a.visited_on, b.visited_on) between 0 and 6
 group by a.visited_on
 having count(b.visited_on) = 7
-
-
