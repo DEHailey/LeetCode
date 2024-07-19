01e75571229631f1637e70d4d1c0ebc6361dbfa8
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-19 14:36:04 +0900

    Time: 414 ms (97.16%), Space: 0B (100.00%) - LeetHub

--- 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
+++ 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
@@ -1,22 +1,12 @@
 # Write your MySQL query statement below
-select 
-    student_id, 
-    min(course_id) as course_id, 
-    grade
-from 
-    Enrollments
-where 
-(student_id, grade) in(
-    select
-        student_id,
-        max(grade)
-    from
-        Enrollments
-    group by
-        student_id
-)
-group by 
-    student_id,
-    grade
-order by 
-    student_id asc
+select student_id, course_id, grade
+from (
+    select 
+        student_id, 
+        course_id, 
+        grade,
+        dense_rank() over(partition by student_id order by grade desc, course_id) as rnk
+    from Enrollments
+) temp
+where rnk = 1
+order by student_id
