8d32011799fdeec26a0c1aebad3d29c201080bdc
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-22 17:04:16 +0900

    Time: 911 ms (54.38%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select seller_name
-from seller s
+from Seller s
 left join Orders o
-on s.seller_id = o.seller_id 
-group by s.seller_id
-having sum(ifnull(year(sale_date) = '2020',0)) = 0
+on o.seller_id = s.seller_id
+group by s.seller_id 
+having sum(ifnull(year(sale_date)=2020,0))=0
 order by 1 asc
