ef6b70bf69d9d727151f597c8bffb5cc117676b8
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-22 17:05:42 +0900

    Time: 845 ms (70.56%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -2,7 +2,7 @@
 select seller_name
 from Seller s
 left join Orders o
-on o.seller_id = s.seller_id
+on s.seller_id = o.seller_id
 group by s.seller_id 
 having sum(ifnull(year(sale_date)=2020,0))=0
 order by 1 asc
