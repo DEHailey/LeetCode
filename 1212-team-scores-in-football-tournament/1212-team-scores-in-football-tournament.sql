f4d94486ec450753aa5147378ac414f3596611cc
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-24 21:12:40 +0900

    Time: 798 ms (43.19%), Space: 0B (100.00%) - LeetHub

--- 1212-team-scores-in-football-tournament/1212-team-scores-in-football-tournament.sql
+++ 1212-team-scores-in-football-tournament/1212-team-scores-in-football-tournament.sql
@@ -1,20 +1,11 @@
 # Write your MySQL query statement below
-select 
-    team_id, 
-    team_name, 
-    sum(case when team_id = host_team and host_goals > guest_goals then 3
-             when team_id = guest_team and guest_goals > host_goals then 3
-             when team_id = host_team and host_goals = guest_goals then 1
-             when team_id = guest_team and guest_goals = host_goals then 1 else 0 end) as num_points
-from 
-    Teams
-left join 
-    Matches
-on 
-    team_id = host_team or team_id = guest_team
-group by
-    team_id
-order by 
-    num_points desc,
-    team_id asc
-    
+select team_id, team_name, 
+       sum(case when team_id = host_team and host_goals > guest_goals then 3
+            when team_id = host_team and host_goals = guest_goals then 1
+            when team_id = guest_team and guest_goals > host_goals then 3
+            when team_id = guest_team and guest_goals = host_goals then 1 else 0 end) as num_points
+from Teams
+left join Matches
+on team_id = host_team or team_id = guest_team
+group by team_id
+order by num_points desc, team_id asc
