efb34b471c8ba44f3bb0715864ce02f410b389ab
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-25 15:32:45 +0900

    Time: 446 ms (25.62%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,9 +1,6 @@
 # Write your MySQL query statement below
 select sale_date, 
-       sum(case when fruit in('apples') then sold_num
-                when fruit in('oranges') then sold_num*-1 end) as diff
+       sum(case when fruit = 'oranges' then sold_num * -1 else sold_num * 1 end) as diff
 from Sales
 group by sale_date
 order by sale_date
-
-
