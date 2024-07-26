4161f95e6fcde0de1ec796d7cee69828be2c6395
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-26 15:00:34 +0900

    Time: 770 ms (52.57%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,9 +1,7 @@
 # Write your MySQL query statement below
-select 
-    u.name,
-    sum(t.amount) as balance
+select u.name as name, sum(t.amount) as balance
 from Users u
 left join Transactions t
 on u.account = t.account
-group by u.name
+group by t.account
 having balance > 10000
