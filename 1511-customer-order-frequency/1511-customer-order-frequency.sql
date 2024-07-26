bafbbc6281018b111c39fd38398e81d946d280b3
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-26 17:19:06 +0900

    Time: 809 ms (77.90%), Space: 0B (100.00%) - LeetHub

--- 1511-customer-order-frequency/1511-customer-order-frequency.sql
+++ 1511-customer-order-frequency/1511-customer-order-frequency.sql
@@ -4,6 +4,6 @@ from Customers
 join Orders using(customer_id)
 join Product using(product_id)
 where year(order_date) = 2020
-group by customer_id
-having sum(if(month(order_date)=6, quantity, 0)*price) >=100 and
-       sum(if(month(order_date)=7, quantity, 0)*price) >= 100
+group by customer_id 
+having sum(if(month(order_date)=6, quantity,0)*price) >= 100 and
+       sum(if(month(order_date)=7, quantity,0)*price) >= 100
