766506569887341089ba176434078c1f58b19452
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-26 17:31:45 +0900

    Time: 992 ms (35.61%), Space: 0B (100.00%) - LeetHub

--- 1495-friendly-movies-streamed-last-month/1495-friendly-movies-streamed-last-month.sql
+++ 1495-friendly-movies-streamed-last-month/1495-friendly-movies-streamed-last-month.sql
@@ -1,9 +1,7 @@
 # Write your MySQL query statement below
-select distinct c.title
+select distinct(c.title) 
 from Content c
-join TVProgram p
-on c.content_id = p.content_id
-where c.Kids_content = 'Y'
-and c.content_type = 'Movies'
-and year(p.program_date) = 2020
-and month(p.program_date) = 6
+join TVProgram t
+on c.content_id = t.content_id
+where year(t.program_date) = 2020 and month(t.program_date) = 6 and c.Kids_content = 'Y' and content_type = 'Movies'
+group by t.content_id
