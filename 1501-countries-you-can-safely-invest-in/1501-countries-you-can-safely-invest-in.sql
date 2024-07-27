b6ae78e3da73e4ffa4a72ade2bf37990569c83f0
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-27 14:02:35 +0900

    Time: 982 ms (83.86%), Space: 0B (100.00%) - LeetHub

--- 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 select c1.name as country
-from Person p
+from Person
 join Country c1
 on substring(phone_number,1,3) = country_code
-join Calls c2
-on p.id in (c2.caller_id, callee_id)
-group by c1.name
-having avg(duration) > (select avg(duration) from calls)
+join Calls
+on id in (caller_id, callee_id)
+group by country
+having avg(duration) > (select avg(duration) from Calls)
