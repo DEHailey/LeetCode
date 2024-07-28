72348557f77fa9363255bb9e501b693d09a7fec8
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-28 15:57:38 +0900

    Time: 818 ms (16.53%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 select distinct page_id as recommended_page
-from(
+from (
     select case when user1_id = 1 then user2_id
                 when user2_id = 1 then user1_id end as user_id
     from Friendship
-) temp
+)temp
 join Likes
 on temp.user_id = Likes.user_id
-where page_id not in (select page_id from Likes where user_id = 1)
+where page_id not in(select page_id from Likes where user_id = 1)
