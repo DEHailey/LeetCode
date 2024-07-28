1cd196ee7b76a6af83fdc3e0439aba8d3b0425da
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-28 18:24:27 +0900

    Time: 1123 ms (97.77%), Space: 0B (100.00%) - LeetHub

--- 1783-grand-slam-titles/1783-grand-slam-titles.sql
+++ 1783-grand-slam-titles/1783-grand-slam-titles.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select player_id,player_name,
-sum(player_id=Wimbledon)+sum(player_id=Fr_open)+sum(player_id= US_open)+sum(player_id=AU_open) as grand_slams_count
+select player_id, player_name,
+       sum(player_id=Wimbledon) + sum(player_id=Fr_open) + sum(player_id=US_open) + sum(player_id=Au_open) as grand_slams_count
 from Players
-join Championships 
-on player_id = Wimbledon or player_id  = Fr_open or player_id = US_open or player_id = Au_open
+join Championships
+on player_id = Wimbledon or player_id = Fr_open or player_id = US_open or player_id = Au_open
 group by player_id
