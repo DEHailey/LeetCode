c3abe70c55a282a904e5ec16355fef18a3f87c6f
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-28 16:10:03 +0900

    Time: 467 ms (45.05%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,13 +1,6 @@
 # Write your MySQL query statement below
-select 
-    id, 
-    if(isnull(p_id),'Root',
-       if(id in(select p_id from Tree), 'Inner', 'Leaf')
-      ) type
+select id, 
+       if(isnull(p_id), 'Root',
+          if(id in(select p_id from Tree), 'Inner', 'Leaf')
+         )as type
 from Tree
-order by id
-
-
-
-
-
