525038ae879a9c238c6561538d751d6ea7712c73
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-29 20:56:06 +0900

    Time: 320 ms (23.23%), Space: 0B (100.00%) - LeetHub

--- 1303-find-the-team-size/1303-find-the-team-size.sql
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
-select employee_id, count(*) over (partition by team_id) as team_size
+select employee_id,
+       count(*) over(partition by team_id) as team_size
 from Employee
