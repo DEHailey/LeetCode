241947a0fb13984b503dfb1b1d99a7e0bc70f5a2
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-31 21:42:44 +0900

    Time: 2446 ms (29.08%), Space: 0B (100.00%) - LeetHub

--- 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
@@ -2,11 +2,11 @@
 select customer_id, product_id, product_name
 from (
     select o.customer_id, p.product_id, p.product_name,
-    rank() over(partition by o.customer_id order by count(p.product_id) desc) as rnk
+           rank() over(partition by o.customer_id order by count(p.product_id) desc)as rnk
     from Orders o
     join Products p
     on o.product_id = p.product_id
     group by customer_id, product_id
-) temp
+)t
 where rnk = 1
-order by customer_id, product_id
+
