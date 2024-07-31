f48c7afda6dac1a6c59ce3fd8176c17190182302
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-31 16:13:16 +0900

    Time: 2102 ms (7.14%), Space: 0B (100.00%) - LeetHub

--- 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
+++ 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
@@ -1,16 +1,10 @@
 # Write your MySQL query statement below
-select customer_name, customer_id, order_id, order_date
-from (
-    select 
-        c.name as customer_name, 
-        c.customer_id,
-        o.order_id,
-        o.order_date,
-        rank() over (partition by c.customer_id order by o.order_date desc) as rnk
-    from
-        Customers c
-    inner join
-        Orders o on c.customer_id = o.customer_id
-) temp
-where rnk <= 3
+select c.name as customer_name, c.customer_id, o.order_id, o.order_date
+from Customers c
+join(
+    select order_id, order_date, customer_id,
+           rank() over(partition by customer_id order by order_date desc) as rnk
+    from Orders
+)o
+on c.customer_id = o.customer_id and rnk <= 3
 order by customer_name, customer_id, order_date desc
