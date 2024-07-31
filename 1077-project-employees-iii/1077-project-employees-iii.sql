26e60bac2b31b24263c44a9101b92ef605224eb8
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-07-31 19:42:52 +0900

    Time: 615 ms (36.84%), Space: 0B (100.00%) - LeetHub

--- 1077-project-employees-iii/1077-project-employees-iii.sql
+++ 1077-project-employees-iii/1077-project-employees-iii.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
-select project_id, employee_id
+select t.project_id, t.employee_id
 from (
-    select 
-        p.project_id, 
-        p.employee_id,
-        dense_rank() over(partition by project_id order by experience_years desc) as rnk
-    from Project p
+    select p.project_id, p.employee_id,
+           rank() over(partition by project_id order by experience_years desc)as rnk
+    from Project p 
     join Employee e
     on p.employee_id = e.employee_id
-) temp
+)t
 where rnk = 1
