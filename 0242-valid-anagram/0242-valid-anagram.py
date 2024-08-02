3134115244c0f4c76878ce0236c377437b11c648
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-08-02 20:48:47 +0900

    Time: 49 ms (58.06%), Space: 16.9 MB (80.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,15 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        s_count = [0] * 26
-        t_count = [0] * 26
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count = [0]*26
+        t_count = [0]*26
         
         for i in range(len(s)):
             s_count[ord(s[i]) - ord('a')] += 1
-
+            
         for i in range(len(t)):
             t_count[ord(t[i]) - ord('a')] += 1
-        
-        return s_count == t_count
-        
+            
+        if s_count == t_count:
+            return True
+        else:
+            return False
