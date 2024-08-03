b8095e0246edb1a908151007c6d8db2b65244460
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-08-04 01:09:20 +0900

    Time: 557 ms (38.59%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -4,7 +4,7 @@ from (
     select case when user1_id = 1 then user2_id
                 when user2_id = 1 then user1_id end as user_id
     from Friendship
-)temp
+)t
 join Likes
-on temp.user_id = Likes.user_id
+on Likes.user_id = t.user_id
 where page_id not in(select page_id from Likes where user_id = 1)
