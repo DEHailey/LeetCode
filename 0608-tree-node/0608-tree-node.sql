faa8e02157133d1f1a0b661bf13311d047bb1d70
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-08-04 01:37:09 +0900

    Time: 503 ms (24.88%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-
 select id,
        if(isnull(p_id), 'Root',
-          if(id in(select p_id from Tree), 'Inner', 'Leaf')
+          if(id in(select p_id from Tree),'Inner','Leaf')
          )as type
 from Tree
