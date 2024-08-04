da769372d2becfca6e517d8b80d8caa54a5099d3
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-08-04 16:39:38 +0900

    Time: 93 ms (51.82%), Space: 16.6 MB (13.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,22 +1,38 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)
-        
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == '.':
-                    continue
-                if (board[r][c] in cols[c] or
-                    board[r][c] in rows[r] or 
-                    board[r][c] in squares[(r//3),(c//3)]):
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Validate Rows
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[i][j]
+                if item in s:
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r//3),(c//3)].add(board[r][c])
-                
+                elif item != '.':
+                    s.add(item)
+        
+        # Validate Cols
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[j][i]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+       
+        # Validate Boxes
+        starts = [(0,0),(0,3),(0,6),
+                  (3,0),(3,3),(3,6),
+                  (6,0),(6,3),(6,6)]
+    
+        for i, j in starts:
+            s = set()
+            for row in range(i, i+3):
+                for col in range(j, j+3):
+                    item = board[row][col]
+                    if item in s:
+                        return False
+                    elif item != '.':
+                        s.add(item)
+                        
         return True
-        
-        
-        
