687839a3654965265b8ceb6c8fd8981fd9949e21
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-08-13 22:34:31 +0900

    Time: 738 ms (15.22%), Space: 0B (100.00%) - LeetHub

--- 1077-project-employees-iii/1077-project-employees-iii.sql
+++ 1077-project-employees-iii/1077-project-employees-iii.sql
@@ -2,8 +2,8 @@
 select t.project_id, t.employee_id
 from (
     select p.project_id, p.employee_id,
-           rank() over(partition by project_id order by experience_years desc)as rnk
-    from Project p 
+           rank() over(partition by project_id order by experience_years desc) as rnk
+    from Project p
     join Employee e
     on p.employee_id = e.employee_id
 )t
