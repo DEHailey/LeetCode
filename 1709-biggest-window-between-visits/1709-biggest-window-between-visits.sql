3c4fa9e728ad0d60cb91eaf8cb1f208d80960ee6
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-08-17 18:30:54 +0900

    Time: 486 ms (74.00%), Space: 0B (100.00%) - LeetHub

--- 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
+++ 1709-biggest-window-between-visits/1709-biggest-window-between-visits.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
-select user_id, max(t.diff) as biggest_window
+select user_id, max(diff) as biggest_window
 from (
-    select user_id, visit_date,
-           datediff(lead(visit_date,1,'2021-1-1') over(partition by user_id order by visit_date), visit_date)as diff
+    select user_id, visit_date, 
+           datediff(lead(visit_date,1,'2021-1-1') over(partition by user_id order by visit_date),visit_date) as diff
     from UserVisits
 )t
 group by user_id
