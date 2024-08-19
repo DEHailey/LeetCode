c3a51c3ed1b8a3b7cc065a38de6c375433bed218
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-08-19 22:55:44 +0900

    Time: 491 ms (66.38%), Space: 0B (100.00%) - LeetHub

--- 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
+++ 1412-find-the-quiet-students-in-all-exams/1412-find-the-quiet-students-in-all-exams.sql
@@ -1,14 +1,13 @@
 # Write your MySQL query statement below
 with qs as(
-    select exam_id, s.student_id, s.student_name, score,
+    select exam_id, Student.student_id, Student.student_name, score, 
            rank() over(partition by exam_id order by score) as rk1,
            rank() over(partition by exam_id order by score desc) as rk2
     from Exam
-    left join Student s
-    on Exam.student_id = s.student_id
+    left join Student
+    on Exam.student_id = Student.student_id
 )
 select distinct student_id, student_name
 from qs
-where student_id not in (select student_id from qs where rk1 = 1 or rk2 = 1)
+where student_id not in (select student_id from qs where rk1=1 or rk2=1)
 order by student_id
-
