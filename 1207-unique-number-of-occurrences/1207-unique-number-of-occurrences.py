d4bd6323a95f5af922567dd30016d6411d06475d
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-08-27 15:02:58 +0900

    Time: 23 ms (38.25%), Space: 11.6 MB (96.11%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        seen = set()
+        
+        for num in collections.Counter(arr).values():
+            if num in seen:
+                return False
+            else:
+                seen.add(num)
+                
+        return True
+        
+        
+        
+        
+        
+        
