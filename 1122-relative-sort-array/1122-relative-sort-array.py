c9230f3d5bdd968aebe490202da8ebcd891db36b
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-09-01 07:51:14 +0900

    Time: 19 ms (70.20%), Space: 11.8 MB (52.04%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -5,13 +5,26 @@ class Solution(object):
         :type arr2: List[int]
         :rtype: List[int]
         """
- 
-        count = Counter(arr1)
-        res = []  
         
+        # make a counter (manually count)
+        count = {}
+        
+        for element in arr1:
+            if element in count:
+                count[element] += 1
+            else:
+                count[element] = 1
+        
+        # put the elements in 'res' depends on how many times in a counter
+        res = []
         for i in arr2:
-            res += [i]*count.pop(i)  
+            res += [i] * count[i]
+            del count[i]
+        
+        # put the elements in 'remain'
+        remain = []
+        for key in count:
+            remain += [key] * count[key]
             
-        return res + sorted(count.elements())
-    
-    
+        # merge 'res' + 'remain'
+        return res + sorted(remain)
