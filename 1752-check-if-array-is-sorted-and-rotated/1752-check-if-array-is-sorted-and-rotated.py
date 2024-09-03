6b11e34feb0417a09eccca192d26bea7ad4e9a08
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-09-03 19:14:57 +0900

    Time: 20 ms (28.82%), Space: 11.7 MB (8.57%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-      
         count = 0
-        n = len(nums)
-
-        for i in range(n):
-            if nums[i] > nums[(i + 1) % n]:
+        if len(nums) < 3:
+            return True
+        
+        for i in range (0,len(nums)):
+            if nums[i] > nums[(i+1) % len(nums)]:
                 count += 1
-            if count > 1:
-                return False
-
-        return True
+                
+        if count == 1 or count == 0:
+            return True
+        else:
+            return False
