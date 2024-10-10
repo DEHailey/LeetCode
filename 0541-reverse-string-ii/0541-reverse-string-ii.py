8fb4d846950f3e54f157ce2ebcb529265adf2ecc
DEHailey <111622572+DEHailey@users.noreply.github.com>
2024-10-11 00:33:17 +0900

    Time: 27 ms (96.67%), Space: 16.7 MB (33.19%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,9 +1,5 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        '''
-        if len(s) < k:
-            return s[::-1]
-        '''
         ans = ''
         for i in range(0,len(s),2*k):
             ans += s[i:i+k][::-1]
