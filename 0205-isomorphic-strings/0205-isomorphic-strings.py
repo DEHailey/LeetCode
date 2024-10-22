657ce6110ca0c2d6fc1a2bf830775840996eace4
Hyunkyung Kim <111622572+DEHailey@users.noreply.github.com>
2024-10-23 00:22:41 +0900

    Time: 5 ms (98.78%), Space: 16.7 MB (93.30%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,12 +1,14 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        s_dic = {}
-        t_dic = {}
+        mapping_s_t = {}
+        mapping_t_s = {}
         
-        for c1, c2 in zip(s, t):
-            if s_dic.get(c1) != t_dic.get(c2):
-                return False
-            s_dic[c1] = t_dic[c2] = c2
-        return True
+        for c1, c2 in zip(s,t):
+            if (c1 not in mapping_s_t) and (c2 not in mapping_t_s):
+                mapping_s_t[c1] = c2
+                mapping_t_s[c2] = c1
             
-        
+            elif mapping_s_t.get(c1) != c2 or mapping_t_s.get(c2) != c1:
+                return False
+            
+        return True
