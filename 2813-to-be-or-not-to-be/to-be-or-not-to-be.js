6eabd007c04c80738b77d4f5bf204b7944eb2f4d
Hyunkyung Kim <111622572+DEHailey@users.noreply.github.com>
2025-11-30 16:06:37 +0900

    Time: 53 ms (18.86%) | Memory: 54.5 MB (17.02%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(other){
+            if (val === other){
+                return true;
+            }
+            else {
+                throw new Error("Not Equal");
+            }
+        },
+
+        notToBe: function(other){
+            if (val !== other) {
+                return true;
+            }
+            else {
+                throw new Error("Equal");
+            }
+        }
+    }
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
